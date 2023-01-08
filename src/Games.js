153eac383e49c7b197cf8402335e1bc40f4a4b64
Zoltan Kiss <zoltan.a.kiss@gmail.com>
2023-01-08 20:50:44 +0100

    mvp

---
+++ src/Games.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { useState, useEffect } from "react";
+
+export default function Games() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+
+   useEffect(() => {
+    fetch(`nhl_games`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(
+            `This is an HTTP error: The status is ${response.status}`
+          );
+        }
+        return response.json();
+      })
+      .then((actualData) => {
+        console.log(actualData)
+        setData(actualData);
+        setError(null);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setData(null);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <ul>
+      <ul>
+        {data &&
+          data.map(({ id, link, status, away_team_name, home_team_name, game_date }) => (
+            <li key={id}>
+              <h1>{away_team_name} vs {home_team_name} ({status} {game_date})</h1>
+              <p>{status}</p>
+            </li>
+          ))}
+      </ul>
+    </ul>
+  );
+}
